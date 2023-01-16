b90ce7d30123d9eabca76f68b7a985998b0d6b32
Jisan901 <jisuna901@gmail.com>
2023-01-16 21:28:09 +0600

    take it first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    return (
+        <div>
+            <Countries></Countries>
+        </div>
+    )
+}
+
+function Countries(){
+    const [rest,setRest] = useState([])
+    
+    useEffect(()=>{
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => {console.log(data); setRest(data)})
+    },[])
+    
+    return (
+    <div>
+        <h1>Countries</h1>
+        <div className="countries">
+            {
+                rest.map(e => <Country name={e.name.common} continent={e.continents[0]} flag={e.flags.png} population={e.population} independent={e.independent?"independent":"not independent"}></Country>)
+            }
+        </div>
     </div>
-  );
+    )
+}
+
+function Country(props){
+    return (
+        <div className="country">
+            <img src={props.flag} alt="flag"/>
+            <h2>{props.name}</h2>
+            <p>continents:{props.continent}</p>
+            <p>population:{props.population}</p>
+            <p>{props.independent}</p>
+        </div>
+    )
 }
 
 export default App;
